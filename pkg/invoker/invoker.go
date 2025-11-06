b1e6e369ea79bf1ffe251bb15981218407b0ef37
livepo <qgmfky@gmail.com>
2025-11-06 19:29:19 +0800

    fix: location

--- pkg/invoker/invoker.go
+++ pkg/invoker/invoker.go
@@ -14,7 +14,7 @@ var (
 
 func Init() {
 	// init db
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Asia%%2FShanghai",
 		config.Cfg.Mysql.User,
 		config.Cfg.Mysql.Password,
 		config.Cfg.Mysql.Host,
